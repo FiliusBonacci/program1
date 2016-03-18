be6a3d7cd3ac9d7acf050cf4f77683f1f6d78c43
Filius Bonacci <FiliusBonacci@interia.pl>
2016-03-18 19:57:42 +0100

    ustepnie ukonczone

--- Osoba.cpp
+++ Osoba.cpp
@@ -35,6 +35,8 @@ void Osoba::przedstaw()
     cout<<"\n------------------------------------------------\n\t";
     cout<<imie<<" "<<nazwisko<<"\nurodzony:\t"<<get_data_urodzenia().get_dzien()<<"."<<get_data_urodzenia().get_miesiac()<<"."<<get_data_urodzenia().get_rok();
     cout<<"\nzamieszkaly: \t" <<get_adres_zamieszkania().get_ulica()<<" "<< get_adres_zamieszkania().get_nr_domu()<<"/"<<get_adres_zamieszkania().get_nr_mieszkania()<<"\n \t\t"<<get_adres_zamieszkania().get_kod() << " " <<get_adres_zamieszkania().get_miasto() <<"\n\t\t"<<get_adres_zamieszkania().get_panstwo()<<"\nzatrudniony w:\n\t\t" << get_zatrudnienie().get_nazwa_firmy()<<endl;
+    cout<<"\t\t"<<get_zatrudnienie().get_adres_firmy().get_ulica()<<" "<<get_zatrudnienie().get_adres_firmy().get_nr_domu()<<endl;
+    cout<<"\t\t"<<get_zatrudnienie().get_adres_firmy().get_kod()<<" "<<get_zatrudnienie().get_adres_firmy().get_miasto()<<endl;
     cout<<"od:\t\t"<<get_zatrudnienie().get_data_zatrudnienia().get_dzien()<<"."<<get_zatrudnienie().get_data_zatrudnienia().get_miesiac()<<"."<<get_zatrudnienie().get_data_zatrudnienia().get_rok()<<endl;
     cout<<"------------------------------------------------\n\n";
 }
